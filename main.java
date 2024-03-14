dac02d51108e77e2f00eb0166a1be1777499c2b2
pratikshyaWD <119840064+pratikshyaWD@users.noreply.github.com>
2024-03-14 18:02:40 +0530

    Initial commit

---
+++ main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("This is java programe");
+        System.out.println("Output from java programe");
+        System.out.println(new Date());
+    }
+}
